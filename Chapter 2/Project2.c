ee563d95342565eb96021410f5c8ac613593e7db
Davi <daczanotelli@edu.unifor.br>
2026-07-25 18:01:26 -0300

    Implement sphere volume calculation in Project2.c

---
+++ Chapter 2/Project2.c
@@ -0,0 +1,16 @@
+/* EXERCISE 2 - Chapter 2 - solved and finished 25/7/2026 at roughly 6:00 PM BRT */
+
+#include <stdio.h>
+#define PI_2DP 3.14f /* Value of pi to two decimal places. */
+
+int main (void) {
+	float sphereVolume;
+	int daRadius;
+
+	daRadius = 10;
+	sphereVolume = (4 / 3) * PI_2DP * daRadius * daRadius * daRadius;
+
+	printf("The volume of a sphere with a radius of %d meters is roughly %.1f cubic meters.\n", daRadius, sphereVolume);
+
+	return 0;
+}
